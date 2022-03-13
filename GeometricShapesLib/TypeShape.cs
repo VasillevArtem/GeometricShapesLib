5db893d4642f513890b39042e9b020d6a4db2cd7
Artem Vasiliev <artvasdmi@gmail.com>
2022-03-13 16:09:36 +0200

    Fix

--- GeometricShapesLib/TypeShape.cs
+++ GeometricShapesLib/TypeShape.cs
@@ -1,37 +1,47 @@
-﻿
-
-namespace GeometricShapesLib
+﻿namespace GeometricShapesLib
 {
     /// <summary>
     /// Статический класс для определения вида конкретной фигуры.
     /// </summary>
     public static class TypeShape
     {
-         /// <summary>
-         /// Статический метод, который определяет вид треугольника: равносторонний, равнобедренный, прямоугольный, разносторонний.
-         /// </summary>
-         /// <param name = "tri"> Экземпляр треугольника.</param>
-         /// <returns>Возвращает строку, описывающую вид треугольника.</returns>
-         public static string GetTypeTriangle(Triangle tri)
+        /// <summary>
+        /// Статический метод, который определяет вид треугольника: равносторонний, равнобедренный, прямоугольный, разносторонний.
+        /// </summary>
+        /// <param name = "triangle"> Экземпляр треугольника.</param>
+        /// <returns>Возвращает строку, описывающую вид треугольника.</returns>
+        public static string GetTypeTriangle(Triangle triangle)
          {
             
-            if (tri.Side_A == tri.Side_B && tri.Side_B == tri.Side_C)
+            if (triangle.Side_A == triangle.Side_B && triangle.Side_B == triangle.Side_C)
                 return "The equilateral triangle";
 
 
-            else if (tri.Side_A == tri.Side_B || tri.Side_B == tri.Side_C || tri.Side_C == tri.Side_A)
+            else if (triangle.Side_A == triangle.Side_B || triangle.Side_B == triangle.Side_C || triangle.Side_C == triangle.Side_A)
                 return "The isosceles triangle";
 
 
-            else if (Math.Pow(tri.Side_A, 2) + Math.Pow(tri.Side_B, 2) == Math.Pow(tri.Side_C, 2) ||
-                     Math.Pow(tri.Side_B, 2) + Math.Pow(tri.Side_C, 2) == Math.Pow(tri.Side_A, 2) ||
-                     Math.Pow(tri.Side_C, 2) + Math.Pow(tri.Side_A, 2) == Math.Pow(tri.Side_B, 2))
+            else if (Math.Pow(triangle.Side_A, 2) + Math.Pow(triangle.Side_B, 2) == Math.Pow(triangle.Side_C, 2) ||
+                     Math.Pow(triangle.Side_B, 2) + Math.Pow(triangle.Side_C, 2) == Math.Pow(triangle.Side_A, 2) ||
+                     Math.Pow(triangle.Side_C, 2) + Math.Pow(triangle.Side_A, 2) == Math.Pow(triangle.Side_B, 2))
 
                 return "The right triangle";
 
 
             return "The versatile triangle";
          }
+
+        public static string GetTypeRectangle(Rectangle rectangle)
+        {
+
+        }
+
+        public static string GetTypeCircle(Circle circle)
+        {
+
+        }
+
+        
     }
     
 }
